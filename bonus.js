60895e466c944fb4c70000d69801c5a1b2446573
Alessandro <marzucoalessandro1@gmail.com>
2020-12-15 18:49:32 +0100

    time function per slider

--- bonus.js
+++ bonus.js
@@ -1,14 +1,3 @@
-// Istruzioni:
-// Ricreare lo slider di immagini ma questa volta con Vue
-// Bonus ufficiale:
-// Facciamo in modo che il nostro slider scorra automaticamente in avanti,
-// e ogni 3 secondi visualizzi un immagine differente.
-// Tip: possiamo usare una lifecycle Hook per inserire un nostro script quando l'istanza Vue é stata caricata.
-// Possibili extra Bonus:
-// cliccando sui pallini visualizzeremo l'immagine corrispondente
-// cliccando sulle frecce della tastiera sinista e destra scorriamo tra le immagini
-// I bonus metteteli in una cartella "bonus".
-
 let app = new Vue ({
   el: ".container",
   data: {
@@ -19,9 +8,11 @@ let app = new Vue ({
       "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg",
       "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg"
     ],
-
   },
 
+  created: function () {
+    setInterval(this.next, 3000)
+  },
 
  // funzioni per scorrere avanti e indietro;
   methods: {
@@ -38,4 +29,5 @@ let app = new Vue ({
      }
    }
  },
+
 });
