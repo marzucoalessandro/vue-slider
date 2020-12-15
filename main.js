a1a382d05d67e275e8b334f81f5e4f2624759b62
Alessandro <marzucoalessandro1@gmail.com>
2020-12-15 16:03:58 +0100

    prima parte esercizio

---
+++ main.js
@@ -0,0 +1,41 @@
+// Istruzioni:
+// Ricreare lo slider di immagini ma questa volta con Vue
+// Bonus ufficiale:
+// Facciamo in modo che il nostro slider scorra automaticamente in avanti,
+// e ogni 3 secondi visualizzi un immagine differente.
+// Tip: possiamo usare una lifecycle Hook per inserire un nostro script quando l'istanza Vue é stata caricata.
+// Possibili extra Bonus:
+// cliccando sui pallini visualizzeremo l'immagine corrispondente
+// cliccando sulle frecce della tastiera sinista e destra scorriamo tra le immagini
+// I bonus metteteli in una cartella "bonus".
+
+let app = new Vue ({
+  el: ".container",
+  data: {
+    counter: 0,
+    slides: [
+      "https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg",
+      "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg",
+      "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg",
+      "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg"
+    ]
+  },
+ // funzioni per scorrere avanti e indietro;
+  methods: {
+    next(){
+      this.counter++;
+      if (this.counter === this.slides.lenght) {
+      this.counter = this.slides.length;
+    } else if (this.counter >= this.slides.length) {
+       this.counter = this.slides.length -4;
+     }
+   },
+   prev(){
+     if (this.counter > 0) {
+       this.counter--
+     } else {
+       this.counter = this.slides.length -1;
+     }
+   }
+ },
+});
